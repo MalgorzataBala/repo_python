ba58953aa31a2c375524af2a6206d068976f4894
szc5kw <malgorzata.wodziak.krk@gmail.com>
2018-09-26 20:08:03 +0200

    update my_first_python.py

---
+++ week1/day2/my_first_python.py
@@ -0,0 +1,28 @@
+# Skryp sprawdza czy liczba jest podzielna przez dwa
+#wczytac dane
+#porownac dane
+#podac wynik
+
+""""
+print ("Podaj liczbe, ktora bedziemy sprawdzac")
+a = input()
+
+if int(a)%2 == 0:
+   print(a," jest liczba podzielna przez 2")
+
+else:
+     print(a,"nie jest liczba podzielna przez 2")
+""""
+""""
+
+input_usera = input("Wpisz liczbę: ")
+input_usera = int(input_usera)
+if input_usera%2 ==0:
+    print("Jest podzielna")
+else:
+    print("Nie jest podzielna")
+
+"""
+
+x="Ala ma kota, kot ma Ale"
+ord("A")
