df77402d811ba3b8f6110b817584b8b3551ae9f6
szc5kw <malgorzata.wodziak.krk@gmail.com>
2018-09-30 22:14:55 +0200

    zadanie 2, choina

---
+++ week1/day2/zad 2.py
@@ -0,0 +1,15 @@
+"""
+Pamiętacie tabele ASCII? Internety tworzą całe obrazy tylko i wyłącznie z podstawowych znaków które dostarcza nam tabela ascii.
+Wystarczy użyć instrukcji `print` i troche pomysłowości w wyświetlaniu spacji i znaków. Może chociaż mała choinka?
+"""
+print("          ,")
+print("         , ,")
+print("        ,,,,,")
+print("       ,     ,")
+print("      ,$$$$$$$,")
+print("     ,,,,,,,,,,,")
+print("    ,           ,")
+print("   ,OoOoOoOoOoOoO,")
+print("  ,,,,,,,,,,,,,,,,,,")
+print("         | |")
+print("         | |")
