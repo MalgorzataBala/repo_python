833917281a3de41ea87c3f9dbecc758ce75a4990
szc5kw <malgorzata.wodziak.krk@gmail.com>
2018-10-15 19:13:01 +0200

    podczas zajec

---
+++ week4/day5/podczas_zajec.py
@@ -0,0 +1,29 @@
+"""def give_square(x,y,z):
+    return y+"xxx", x**2, z+z
+
+
+wywolanie = give_square(2,"zz", [1,2,3,4,5])
+print(wywolanie)
+
+def foo(imie, nazwisko):
+    imie = imie.capitalize()
+    nazwisko = nazwisko.capitalize()
+    return f"{imie} \t {nazwisko}"
+
+napis=foo("celina","eugenia")
+print(napis)
+"""
+#temat na rozmowie kwalifikacyjnej - bez cleara do listy ciagle beda dodawane argumenty
+
+def dodaj_imie(imie, imiona = None):
+    if imiona == None:
+        imiona = []
+    imiona.append(imie)
+    return imiona
+
+i=[]
+dodaj_imie("Adam",i)
+dodaj_imie("Monia",i)
+dodaj_imie("Jurek",i)
+
+print(i)
