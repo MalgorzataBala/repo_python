9397419fc3d9578f746d6c82a83b2ffb276b1583
szc5kw <malgorzata.wodziak.krk@gmail.com>
2018-10-15 14:45:13 +0200

    homework

---
+++ zadanka.py
@@ -0,0 +1,20 @@
+#zad. 1 tworzymy liste i pozbywamy sie z niej duplikatów
+input_usera=[1,2,3,4,1]
+#input_usera=input("Podaj jakąś liste")
+el_listy=0
+for el_listy in range(input_usera):
+    a=0
+    for a in range(input_usera):
+        if el_listy ==input_usera(a):
+            el_listy.remove(a)
+    a=a+1
+
+
+
+
+
+
+#zad.2 sumator elementów listy
+
+
+#zad.3 max/min
