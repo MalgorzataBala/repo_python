aa2c1efcc93d21810fdb495f4636ebd35d445771
szc5kw <malgorzata.wodziak.krk@gmail.com>
2018-10-15 20:11:15 +0200

    podczas zajec 3

--- week4/day5/podczas_zajec.py
+++ week4/day5/podczas_zajec.py
@@ -47,10 +47,53 @@ def i():
 
 i()
 print(" NA ZEWNATRZ",imie)
-"""
+
 
 imiona=[]
 def i():
     imiona.append("Ania")
 i()
 print(" NA ZEWNATRZ",imiona)
+
+"""
+
+def sumator(lista_suma):
+    suma = 0
+    for i in lista_suma:
+        suma = suma + i
+    return suma
+
+ista_suma = [1, 2, 3, 4, 5, 6]
+x = sumator(ista_suma)
+print(x)
+
+
+#funkcja sprawdzajace czy podany napis jest palindromem
+
+def palindrom(slowo):
+    slowo = slowo.lower()
+    len_slowo = len(slowo)-1
+    j = len_slowo
+    k = 0
+    while k in range(len_slowo):
+        if k != j:
+            if slowo[k] == slowo[j]:
+                j=len_slowo-1
+                k = k + 1
+            else:
+                slowo= slowo+" nie"
+        k = len_slowo
+    return slowo +" jest palindromem"
+
+z = palindrom("kamil ślimak")
+print(z)
+
+
+def trywial_palindrom(slowoo):
+    if slowoo == slowoo[::-1]:
+        return slowoo+" jest palidromem"
+    else:
+        return slowoo+" nie jest palindromem"
+
+m= trywial_palindrom("kajafgdgk")
+print(m)
