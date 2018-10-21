216800853d2d14a4bfa0e940d380c1138dceba2a
szc5kw <malgorzata.wodziak.krk@gmail.com>
2018-10-21 20:19:41 +0200

    multiplikator

---
+++ week4/day6/multiplikator.py
@@ -0,0 +1,13 @@
+#Multiplikator czyli na wzór sumatora chcemy przemnożyć wszystkie liczby z zadanej listy.
+# Nie kopiujemy próbujemy napisać sami
+
+def multiplikator(zakres):
+    silnia = 1
+    for i in zakres:
+        silnia = silnia*i
+    return silnia
+
+x = int(input("Podaj jakąć liczbę, a ja obliczę jej silnie: "))
+zakres_usera  = range(1, x+1)
+z = multiplikator(zakres_usera)
+print(z)
