f73334d2657b4621e60b69688ff0e6546b2f830a
szc5kw <malgorzata.wodziak.krk@gmail.com>
2018-10-24 20:01:52 +0200

    class

---
+++ week4/week5/day 10/engines.py
@@ -0,0 +1,7 @@
+
+class Silniki(object):
+    def __init__(self, ppojemnosc, rodzaj, konie):
+        self.pojemnosc = ppojemnosc
+        self.rodzaj_silnika = rodzaj
+        self.ile_koni = konie
+
---
+++ week4/week5/day 10/klasy_itp.py
@@ -0,0 +1,29 @@
+"""class Samochod(object):
+    def __init__(self, user_model, engine_type, user_predkosc = 10):
+        print('I am in init!')
+        self.model = user_model
+        self.engine = engine_type
+
+
+    def zatrab(self):
+        print('Honk Honk')
+
+    def przyspiesz(self,wartosc):
+        self.predkosc = self.predkosc + wartosc
+
+
+
+#print(Samochod)
+samochod1 = Samochod('VW', "Fuel") #tworze instancje
+
+#samochod1.model = "VW"
+print(samochod1.model)
+print(samochod1.engine)
+print(samochod1.przyspiesz(2))
+
+samochod1.zatrab()
+samochod2 = Samochod('Audi', "Fuel")
+print(samochod2.model)
+
+"""
+
